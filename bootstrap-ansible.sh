99a48dcd6dcc6f78925991f055cae2632d779593
horseinthesky <pwnedbyspawn@gmail.com>
2018-05-01 11:19:08 +0000

    bootstrap fix

---
+++ bootstrap-ansible.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NORMAL='\033[0m'
+
+printf "${CYAN}Install base packages...${NORMAL}\n"
+sudo apt update -y
+sudo apt install python3-pip -y
+sudo pip3 install -U pip
+sudo pip install cryptography
+sudo pip install --ignore-installed pyyaml
+sudo pip install ansible
+printf "${GREEN}DONE!${NORMAL}\n"
