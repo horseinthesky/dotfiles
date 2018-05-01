65805b5f1057a8ab6fee325c641e52be27f89d78
horseinthesky <pwnedbyspawn@gmail.com>
2018-05-01 11:36:10 +0000

    remove --ignore-installed

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ printf "${GREEN}DONE!${NORMAL}\n"
 
 printf "${CYAN}Installing python3 modules...\n${NORMAL}"
 sudo pip3 install -U pip
-sudo pip install --ignore-installed -U -r requirements.txt
+sudo pip install -U -r requirements.txt
 printf "${GREEN}DONE!${NORMAL}\n"
 
 printf "${CYAN}Install oh-my-zsh and plugins...${NORMAL}\n"
