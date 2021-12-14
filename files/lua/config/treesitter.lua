3f136f06b80d6cfb2536b1207c03c0e2b13233be
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2021-12-15 00:09:43 +0300

    treesitter disable indent

--- files/lua/config/treesitter.lua
+++ files/lua/config/treesitter.lua
@@ -25,10 +25,12 @@ require("nvim-treesitter.configs").setup {
   ensure_installed = "maintained",
   highlight = {
     enable = true,
+    disable = { "json" },
+    additional_vim_regex_highlighting = false,
   },
   indent = {
-    enable = true,
-    disable = { "yaml" },
+    enable = false,
+    -- disable = { "yaml" },
   },
   incremental_selection = {
     enable = true,
