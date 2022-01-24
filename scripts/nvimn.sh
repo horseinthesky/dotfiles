9965a9e16ea0d85c655cc40ffa756008d0a22521
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-01-24 22:49:47 +0300

    removed packer script install

--- scripts/nvimn.sh
+++ scripts/nvimn.sh
@@ -43,25 +43,9 @@ make \
   CMAKE_INSTALL_PREFIX=$HOME/.local install 1> /dev/null
 echo -e "${GREEN}Done${NORMAL}"
 
-echo -e "\n${LIGHTMAGENTA}Installing packer...${NORMAL}"
-PACKER_PATH=$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-if [[ ! -d $PACKER_PATH ]]; then
-  git clone https://github.com/wbthomason/packer.nvim \
-    $PACKER_PATH
-else
-  echo -e "${YELLOW}Already exists${NORMAL}"
-fi
-
 echo -e "\n${LIGHTMAGENTA}Setting up neovim...${NORMAL}"
 [[ ! -d $HOME/.config/nvim ]] && mkdir -p $HOME/.config/nvim
 symlink $DOTFILES_HOME/init.vim $HOME/.config/nvim/init.vim
-symlink $DOTFILES_HOME/coc-settings.json $HOME/.config/nvim/coc-settings.json
 symlink $DOTFILES_HOME/lua $HOME/.config/nvim/lua
 symlink $DOTFILES_HOME/UltiSnips $HOME/.config/nvim/UltiSnips
 echo -e "${GREEN}Done${NORMAL}"
-
-# echo -e "\n${LIGHTMAGENTA}Setup Neovim plugins...${NORMAL}"
-# if [[ $(echo $($HOME/.local/bin/nvim --version | head -n 1 | cut -c7-11) 0.5.0 | awk '{if ($1 >= $2) print "1"; else print ""}') ]]; then
-#   $HOME/.local/bin/nvim -u NORC +"lua require 'plugins'" +PackerInstall +qa
-# fi
-# echo -e "${GREEN}Done${NORMAL}"
