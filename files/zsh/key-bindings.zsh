14c8ad500ced985846f9a714e3a0de32ef2313df
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-02-28 21:36:27 +0300

    refactor: zsh terminfo keymaps

--- files/zsh/key-bindings.zsh
+++ files/zsh/key-bindings.zsh
@@ -2,81 +2,61 @@
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
 # http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
 
-# Make sure that the terminal is in application mode when zle is active, since
-# only then values from $terminfo are valid
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-  function zle-line-init() {
-    echoti smkx
-  }
-  function zle-line-finish() {
-    echoti rmkx
-  }
-  zle -N zle-line-init
-  zle -N zle-line-finish
-fi
-
 # Use emacs key bindings
 bindkey -e
 
+# +------------------------------------+
+# | Using terminfo in Application Mode |
+# +------------------------------------+
+
+typeset -g -A key
+
+key[Backspace]="${terminfo[kbs]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+key[CTRLL]="${terminfo[kLFT5]}"
+key[CTRLR]="${terminfo[kRIT5]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+
+# Setup key accordingly
+[[ -n "${key[Backspace]}" ]] && bindkey -- "${key[Backspace]}" backward-delete-char
+[[ -n "${key[Shift-Tab]}" ]] && bindkey -- "${key[Shift-Tab]}" reverse-menu-complete
+[[ -n "${key[CTRLL]}" ]] && bindkey -- "${key[CTRLL]}" backward-word
+[[ -n "${key[CTRLR]}" ]] && bindkey -- "${key[CTRLR]}" forward-word
+
 # Start typing + [Up-Arrow] - fuzzy find history forward
-if [[ -n "${terminfo[kcuu1]}" ]]; then
+if [[ -n "${key[Up]}" ]]; then
   autoload -U up-line-or-beginning-search
   zle -N up-line-or-beginning-search
 
-  bindkey -M emacs "${terminfo[kcuu1]}" up-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
-  bindkey -M vicmd "${terminfo[kcuu1]}" up-line-or-beginning-search
+  bindkey -- "${key[Up]}" up-line-or-beginning-search
 fi
 
 # Start typing + [Down-Arrow] - fuzzy find history backward
-if [[ -n "${terminfo[kcud1]}" ]]; then
+if [[ -n "${key[Down]}" ]]; then
   autoload -U down-line-or-beginning-search
   zle -N down-line-or-beginning-search
 
-  bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
-  bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
-  bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
+  bindkey -- "${key[Down]}" down-line-or-beginning-search
 fi
-
-# [Shift-Tab] - move through the completion menu backwards
-if [[ -n "${terminfo[kcbt]}" ]]; then
-  bindkey -M emacs "${terminfo[kcbt]}" reverse-menu-complete
-  bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
-  bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
-fi
-
-# [Backspace] - delete backward
-bindkey -M emacs '^?' backward-delete-char
-bindkey -M viins '^?' backward-delete-char
-bindkey -M vicmd '^?' backward-delete-char
-
-# [Ctrl-RightArrow] - move forward one word
-bindkey -M emacs '^[[1;5C' forward-word
-bindkey -M viins '^[[1;5C' forward-word
-bindkey -M vicmd '^[[1;5C' forward-word
-
-# [Ctrl-LeftArrow] - move backward one word
-bindkey -M emacs '^[[1;5D' backward-word
-bindkey -M viins '^[[1;5D' backward-word
-bindkey -M vicmd '^[[1;5D' backward-word
 
 # [Ctrl-r] - Search backward incrementally for a specified string.
 # The string may begin with ^ to anchor the search to the beginning of the line.
-bindkey '^r' history-incremental-search-backward
+bindkey -- "^r" history-incremental-search-backward
 
 # [Space] - don't do history expansion
-bindkey ' ' magic-space
+bindkey -- " " magic-space
 
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line
+bindkey -- "\C-x\C-e" edit-command-line
 
 # Copy previous shell work (useful when renaming)
-bindkey "^p" copy-prev-shell-word
+bindkey -- "^p" copy-prev-shell-word
 
 # Put sudo in front of command: bindkey <Esc><Esc>
-sudo-command-line () {
+_sudo-command-line () {
   # Use last command if no command given
   [[ -z $BUFFER ]] && LBUFFER=$(fc -ln -1)
 
@@ -98,8 +78,17 @@ sudo-command-line () {
   LBUFFER=${WHITESPACE}${LBUFFER}
 }
 
-zle -N sudo-command-line
+zle -N _sudo-command-line
 
-bindkey -M emacs '\e\e' sudo-command-line
-bindkey -M vicmd '\e\e' sudo-command-line
-bindkey -M viins '\e\e' sudo-command-line
+bindkey -- "\e\e" _sudo-command-line
+
+# Make sure that the terminal is in application mode when zle is active.
+# Only then values from $terminfo are valid
+# Downside: when a CLI / TUI doesn't use application mode, some keys won't work.
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+  autoload -Uz add-zle-hook-widget
+  function zle_application_mode_start { echoti smkx }
+  function zle_application_mode_stop { echoti rmkx }
+  add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+  add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
