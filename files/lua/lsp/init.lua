636bc46d61c7da8d57d51903ad3bf460543c4705
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2022-04-24 18:29:15 +0300

    fix: <buffer> for document diagnostics pattern

--- files/lua/lsp/init.lua
+++ files/lua/lsp/init.lua
@@ -26,11 +26,13 @@ local on_attach = function(client, _)
     local lsp_highlight_group = vim.api.nvim_create_augroup("LSPHighlight", { clear = true })
 
     vim.api.nvim_create_autocmd("CursorHold", {
+      pattern = "<buffer>",
       callback = vim.lsp.buf.document_highlight,
       group = lsp_highlight_group,
     })
 
     vim.api.nvim_create_autocmd("CursorMoved", {
+      pattern = "<buffer>",
       callback = vim.lsp.buf.clear_references,
       group = lsp_highlight_group,
     })
