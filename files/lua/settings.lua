c75922d5bef9c9091c6eea04a19c90ead7afdee3
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2023-01-09 22:11:01 +0300

    update: last pos autocommand fully lua version

--- files/lua/settings.lua
+++ files/lua/settings.lua
@@ -192,10 +192,11 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Return to last edit position when opening files (You want this!)
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
-    local last_pos = vim.fn.line "'\""
+    local last_pos = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
 
-    if last_pos >= 1 and last_pos <= vim.fn.line "$" then
-      vim.cmd [[normal! g`"]]
+    if last_pos[1] > 0 and last_pos[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, last_pos)
     end
   end,
 })
