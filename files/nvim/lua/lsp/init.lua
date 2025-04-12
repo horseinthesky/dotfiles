8b4d5aa77173fffd215742bdeb7ebb3cad9bea36
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2025-04-12 12:05:16 +0300

    update: lsp configs auto discovery

--- files/nvim/lua/lsp/init.lua
+++ files/nvim/lua/lsp/init.lua
@@ -3,21 +3,17 @@ require "lsp.diagnostic"
 require "lsp.on_attach"
 local keymaps = require "lsp.keymaps"
 
+-- Shared LSP config
 vim.lsp.config("*", {
   root_markers = { ".git" },
   on_attach = keymaps.set,
 })
 
-vim.lsp.enable {
-  "bashls",
-  "yamlls",
-  "jsonls",
-  "dockerls",
-  "terraformls",
-  "buf_ls",
-  "lua_ls",
-  "pyright",
-  "ruff",
-  "gopls",
-  "rust_analyzer",
-}
+-- Auto discover LSP configs
+local lsp_configs = {}
+for _, filename in pairs(vim.api.nvim_get_runtime_file('lsp/*.lua', true)) do
+  local server_name = vim.fn.fnamemodify(filename, ':t:r')
+  table.insert(lsp_configs, server_name)
+end
+
+vim.lsp.enable(lsp_configs)
