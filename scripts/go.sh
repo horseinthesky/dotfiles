575cea0d14ca380fe5db6960523175cca9b1f5ae
Kirill 'horseinthesky' Pletnev <pwnedbyspawn@gmail.com>
2025-07-28 11:55:21 +0300

    update: go 1.24.2

--- scripts/go.sh
+++ scripts/go.sh
@@ -34,7 +34,7 @@ install_go () {
       ;;
   esac
 
-  local version=1.24.0
+  local version=1.24.2
   local tarball=go${version}.linux-$arch.tar.gz
 
   if [[ -n $(command -v go) ]] && [[ $(go version | awk '{print $3}' | cut -c3-) == $version ]]; then
